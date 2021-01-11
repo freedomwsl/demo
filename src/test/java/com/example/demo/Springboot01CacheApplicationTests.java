fb197c9c706674e7baec0211de6aebdff9d4df60
王跃宇 <yueyu.wang@hand-china.com>
2021-01-11 18:39:30 +0800

    二次提交

--- src/test/java/com/example/demo/Springboot01CacheApplicationTests.java
+++ src/test/java/com/example/demo/Springboot01CacheApplicationTests.java
@@ -176,6 +176,7 @@ public class Springboot01CacheApplicationTests {
         results.forEach(System.out::println);
         for(int i=0;i<100;i++){
             System.out.println(i);
+            System.out.println(i);
         }
     }
 }
